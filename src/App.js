43e21d900151907fd54be6557086f72236a19cb1
Victor <victorvpn13@gmail.com>
2020-12-23 12:10:47 -0300

    Alterações na organização do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import useState from 'react';
 import MainMenu from './pages/MainMenu';
+import Game from './pages/Game';
+import { generateQuestions, checkAnswer } from './functions';
 
-function App() {
+export default function App() {
+  //const [index, setIndex] = useState(0);
+  //const [errors, setErrors] = useState(0);
+  //const [time, setTime] = useState(parseInt(new Date().getTime()));
+
+  let question = generateQuestions(2)[0];
+  
+  console.log(question);
   return (
-    <MainMenu />
+    <Game question={question} />
   );
 }
-
-export default App;
