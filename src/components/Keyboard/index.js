d6628a93ccfbe8775bf6bd527366e1b2f7c57bfd
Victor <victorvpn13@gmail.com>
2020-12-23 12:09:43 -0300

    Cria o componente Keyboard

---
+++ src/components/Keyboard/index.js
@@ -0,0 +1,23 @@
+import './styles.css';
+
+export default function Keyboard() {
+    return (
+        <div className="keyboard">
+            <button className="btn-keyboard">1</button>
+            <button className="btn-keyboard">2</button>
+            <button className="btn-keyboard">3</button>
+
+            <button className="btn-keyboard">4</button>
+            <button className="btn-keyboard">5</button>
+            <button className="btn-keyboard">6</button>
+
+            <button className="btn-keyboard">7</button>
+            <button className="btn-keyboard">8</button>
+            <button className="btn-keyboard">9</button>
+
+            <button className="btn-keyboard">0</button>
+            <button className="btn-keyboard">X</button>
+            <button className="btn-keyboard">^</button>
+        </div>
+    );
+}
